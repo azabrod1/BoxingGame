108bb5b6ce5469f500a26eb6fdadab34f150fc41
vzabrodskiy <vzabrodskiy@gmail.com>
2020-05-08 14:35:03 +0200

    Update

--- First/FightOutcome/MethodOfResult.cs
+++ First/FightOutcome/MethodOfResult.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 namespace Fighting
 {
+
+    public enum Result
+	{
+        BeforeFight = 0,
+        Won = 1, // by first fighter
+        Lost = 2,
+        Draw = 3,
+        Canceled = 4,
+        InProgress = 5
+
+
+    };
+
     public enum MethodOfResult
     {
         KO,
