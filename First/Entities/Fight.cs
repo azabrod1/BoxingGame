644678cbbbe26dc0712355fc1149d41a044997e5
vzabrodskiy <vzabrodskiy@gmail.com>
2020-05-08 13:07:25 +0200

    Only added a comment

--- First/Entities/Fight.cs
+++ First/Entities/Fight.cs
@@ -20,8 +20,10 @@ namespace Main
             this.b1 = b1;
             this.b2 = b2;
             this.roundsScheduled = roundsScheduled;
-        }  
 
+
+        }  
+        // testo 
 
     }
 }
