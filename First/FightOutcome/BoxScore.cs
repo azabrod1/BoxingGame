d2a9cccc42b65759e1d711608cec319ccfa13c9d
azabrod1 <alex.zabrodskiy86@gmail.com>
2020-05-08 01:06:19 +0100

    new file location

---
+++ First/FightOutcome/BoxScore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fighting
+{
+
+    public class BoxScore
+    {
+        public int Result; //Minute the fight ended
+        public List<double> Damage;
+        public List<double> PunchesLanded;
+        public List<double> PunchesThrown;
+
+        public BoxScore()
+        {
+            this.Result = -1;
+            this.Damage = new List<double>();
+            this.PunchesLanded = new List<double>();
+            this.PunchesThrown = new List<double>();
+
+        }
+
+        public double LandedPercent()
+        {
+            return PunchesLanded.Sum() / PunchesThrown.Sum();
+
+        }
+
+        public double TotalDamage()
+        {
+            return Damage.Sum();
+        }
+
+        public double AvgDamage()
+        {
+            return Damage.Average();
+        }
+
+        public void AppendData(int round, double damage, double punchesThrown, double punchesLanded){
+            while(Damage.Count() <= round)
+            {
+                Damage.Add(0);
+                PunchesLanded.Add(0);
+                PunchesThrown.Add(0);
+            }
+
+            Damage[round] += damage;
+            PunchesThrown[round] += punchesThrown;
+            PunchesLanded[round] += punchesLanded;
+        }
+       
+        public override string ToString()
+        {
+            return String.Format($"Result: {Result} Landed % {LandedPercent()}, Total Damage: {TotalDamage()}");
+        }
+    }
+}
