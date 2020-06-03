7bc23d066195a43da124f54c495b6572b97b4157
vzabrodskiy <vzabrodskiy@gmail.com>
2020-06-03 18:59:12 +0200

    Added basic functionality for FighterPopularity

--- First/FighterRanking/FighterPopularity.cs
+++ First/FighterRanking/FighterPopularity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using Main;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 
 namespace Boxing.FighterRanking
@@ -22,9 +23,10 @@ namespace Boxing.FighterRanking
                 this.Coefficient = Coefficient;
             }
 
-            public double Base {
+            public double Base
+            {
                 get => Fans + Followers;
-                }
+            }
 
 
         }
@@ -37,6 +39,25 @@ namespace Boxing.FighterRanking
         {
             PopularityStruct p = new PopularityStruct(100, 100, 1);
             return Popularity.TryAdd(fighter.Name, p);
+        }
+
+        public void AddFighters(IEnumerable<Fighter> fighters)
+        {
+            foreach (var fighter in fighters)
+                AddFighter(fighter);
+        }
+
+        public double Coefficient(Fighter f) => Popularity[f.Name].Coefficient;
+
+        public double Fans(Fighter f) => Popularity[f.Name].Fans;
+
+        public double Followers(Fighter f) => Popularity[f.Name].Followers;
+
+        public double Base(Fighter f) => Popularity[f.Name].Base;
+
+        public double CalculatePopularityChange(Fighter f1, Fighter f2, double score)
+        {
+            return 0;
         }
 
 
