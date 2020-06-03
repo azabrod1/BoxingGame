55d8cd88ceab64cc617b509d34e85ca8c82dd84f
vzabrodskiy <vzabrodskiy@gmail.com>
2020-06-03 17:16:55 +0200

    New class

---
+++ First/FighterRanking/FighterPopularity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Main;
+using Newtonsoft.Json;
+
+
+namespace Boxing.FighterRanking
+{
+    public class FighterPopularity
+    {
+
+
+
+    }
+}
