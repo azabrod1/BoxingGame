c4e6d367cc826df298952106131c2842c03a3112
vzabrodskiy <vzabrodskiy@gmail.com>
2020-07-11 17:47:46 +0200

    Fixed XML duplicates

--- First/Entities/Venue.cs
+++ First/Entities/Venue.cs
@@ -7,24 +7,30 @@ using Main;
 
 
 
-namespace Boxing.Entities
+namespace Entities
 {
     public class Venue
     {
 
         public string Name { get; }              //Country Full Name - the identifier
-        public string ShortName { get; }         //GUI Friendly Short Name <= 3 characters
-        public double elasticity { get; }
-        public double capacity { get; }
+        //public string ShortName { get; }         //GUI Friendly Short Name <= 3 characters
+        public double Elasticity { get; }
+        public double Capacity { get; }
+        public string City { get; }
+        public string State { get; }
 
         public static Dictionary<string, Venue> Venues { get; internal set; }
         //public static double VENUE_FREQ_SUM;
 
-        public Venue(string name, string displayName, double elasticity, double capacity)
+        public Venue(string name, double elasticity, double capacity, string city, string state)
         {
 
             this.Name = name;
-            this.ShortName = displayName;
+            this.Elasticity = elasticity;
+            this.Capacity = capacity;
+            this.City = city;
+            this.State = state;
+            //this.ShortName = displayName;
 
             
         }
@@ -38,7 +44,10 @@ namespace Boxing.Entities
 
 
         private static Dictionary<string, Venue> LoadVenues()
+
         {
+
+
             var assembly = Assembly.GetExecutingAssembly();
             string resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("FightConfig.xml"));
             XElement config = null;
@@ -49,18 +58,24 @@ namespace Boxing.Entities
                     throw new NullReferenceException("Unable to load FightConfig.xml");
             }
 
-            Dictionary<string, Venue> _venues = (from location in config.Descendants("Venue")
+            //Console.WriteLine("In LoadVenues...");
+
+            Dictionary<string, Venue> _venues = (from v in config.Descendants("Venue")
                                                       select new
                                                       {
-                                                          Name = location.Element("Name").Value,
-                                                          ShortName = location.Element("ShortName").Value,
-                                                          Elasticity = double.Parse(location.Element("Elasticity").Value),
-                                                          Capacity = double.Parse(location.Element("Capacity").Value),
+                                                          Name = v.Element("Name").Value,
+                                                          Elasticity = double.Parse(v.Element("Elasticity").Value),
+                                                          Capacity = double.Parse(v.Element("Capacity").Value),
+                                                          City = v.Element("City").Value,
+                                                          State = v.Element("State").Value
                                                       })
                                   .ToDictionary(
                                         structure => structure.Name,
-                                        structure => new Venue(structure.Name, structure.ShortName, structure.Elasticity, structure.Capacity)
+                                        structure => new Venue(structure.Name, structure.Elasticity, structure.Capacity,
+                                                structure.City, structure.State)
                                   );
+            //Console.WriteLine("Number of venues = " + _venues.Count);
+
             return _venues;
         }
 
